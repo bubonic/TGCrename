49bd5807c1f750e596dda0ced27d0b73cdcbc0a8
bubonic <bubonic@tessellations.net>
2017-08-12 03:57:53 +0200

    Fixed _ bug

--- TGCrename.sh
+++ TGCrename.sh
@@ -144,7 +144,7 @@ while [ "$i" -lt "${#LFILES[@]}" ]; do
 #                mv "${LFILES[$i]}" "`echo ${LFILES[$i]} | sed 's/.*[Ll][Ee][Cc][Tt]/g'`"
                 mv "${LFILES[$i]}" "`echo ${LFILES[$i]} | perl -p -e 's/.*lect//gi'`"
         fi
-	temp=`echo ${LFILES[$i]} | grep -i "_[0-9][0-9]"`
+	temp=`echo ${LFILES[$i]} | grep -i "_[0-9]{4}"`
 	result=$?
 	if [ "$result" -eq "0" ]; then
 		mv "${LFILES[$i]}" "`echo ${LFILES[$i]} | perl -p -e 's/.*_[0-9]{4}//gi'`"
